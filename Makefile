df2399ae27b559272e77bf4549c923b3a984cf0b
Alex Leith <alex@auspatious.com>
2018-01-22 14:10:59 +1100

    working mbtiles test

--- Makefile
+++ Makefile
@@ -22,3 +22,22 @@ load-data:
 		&& ogr2ogr -f "ESRI Shapefile" -overwrite -t_srs "EPSG:4326" \
 			data_dir/data/shapefiles/list_parcels_hobart_4326.shp ./tmp/list_parcels_hobart.shp \
 		&& rm -rf tmp/list_parcels_hobart*
+
+convert-data:
+	ogr2ogr -f "GeoJSON" -overwrite -t_srs "EPSG:4326" \
+			mbtiles/list_parcels_hobart.json ./tmp/list_parcels_hobart.shp
+
+make-mbtiles:
+	docker run --rm -v $(shell pwd)/mbtiles:/data alexgleith/tippecanoe \
+		tippecanoe -o /data/list_parcels_hobart.mbtiles \
+			--force \
+			--maximum-zoom=16 \
+			--coalesce-smallest-as-needed \
+			--detect-shared-borders \
+			--no-tile-size-limit \
+			--no-feature-limit \
+			-l list_parcels_hobart -n "Properties in Hobart" /data/list_parcels_hobart.json
+
+mbtiles-bash:
+	docker run -it -v $(shell pwd)/mbtiles:/data alexgleith/tippecanoe \
+		bash
--- webapp/src/components/HelloWorld.vue
+++ webapp/src/components/HelloWorld.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="map">
+  <div>
+    <nav id="menu"></nav>
+    <div id="map"></div>
   </div>
 </template>
 
@@ -12,6 +14,8 @@ export default {
     var startCenter = [147.31, -42.89]
     var startZoom = 13
     var layerWMTS = 'http://localhost:8080/geoserver/gwc/service/wmts?layer=public:list_parcels_hobart_4326&tilematrixset=EPSG:900913&Service=WMTS&Request=GetTile&Version=1.0.0&Format=application/x-protobuf;type=mapbox-vector&TileMatrix=EPSG:900913:{z}&TileCol={x}&TileRow={y}'
+    var layerMBTiles = 'http://127.0.0.1:5000/list_parcels_hobart/{z}/{x}/{y}'
+   
     var map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v9',
@@ -30,8 +34,9 @@ export default {
         }
       }
 
+      // Geoserver layer
       map.addLayer({
-        'id': 'test-layer',
+        'id': 'geoserver',
         'type': 'fill',
         'source': {
           'type': 'vector',
@@ -39,14 +44,38 @@ export default {
             layerWMTS
           ]
         },
+        'layout': {
+          'visibility': 'none'
+        },
         'source-layer': 'list_parcels_hobart_4326',
         'paint': {
           'fill-color': 'rgba(244, 131, 66, 0.4)',
           'fill-outline-color': 'rgba(244, 131, 66, 0.9)'
         }
       }, firstSymbolId)
+
+      // MBTiles layer
+      map.addLayer({
+        'id': 'mbtiles',
+        'type': 'fill',
+        'source': {
+          'type': 'vector',
+          'tiles': [
+            layerMBTiles
+          ]
+        },
+        'layout': {
+          'visibility': 'visible'
+        },
+        'source-layer': 'list_parcels_hobart',
+        'paint': {
+          'fill-color': 'rgba(66, 101, 244, 0.4)',
+          'fill-outline-color': 'rgba(66, 101, 244, 0.9)'
+        }
+      }, firstSymbolId)
     })
-    map.on('click', 'test-layer', function (e) {
+
+    map.on('click', 'geoserver', function (e) {
       if (e.features.length > 0) {
         new mapboxgl.Popup()
           .setLngLat(e.lngLat)
@@ -54,6 +83,91 @@ export default {
           .addTo(map)
       }
     })
+
+    map.on('click', 'mbtiles', function (e) {
+      if (e.features.length > 0) {
+        new mapboxgl.Popup()
+          .setLngLat(e.lngLat)
+          .setHTML('<p>PID: ' + e.features[0].properties.PID + '</p>')
+          .addTo(map)
+      }
+    })
+
+    var toggleableLayerIds = [ 'geoserver', 'mbtiles' ]
+
+    for (var i = 0; i < toggleableLayerIds.length; i++) {
+      var id = toggleableLayerIds[i]
+
+      var link = document.createElement('a')
+      link.href = '#'
+      link.className = 'active'
+      if (i === 0) {
+        link.className = ''
+      }
+      link.textContent = id
+
+      link.onclick = function (e) {
+        var clickedLayer = this.textContent
+        e.preventDefault()
+        e.stopPropagation()
+
+        var visibility = map.getLayoutProperty(clickedLayer, 'visibility')
+
+        if (visibility === 'visible') {
+          map.setLayoutProperty(clickedLayer, 'visibility', 'none')
+          this.className = ''
+        } else {
+          this.className = 'active'
+          map.setLayoutProperty(clickedLayer, 'visibility', 'visible')
+        }
+      }
+
+      var layers = document.getElementById('menu')
+      layers.appendChild(link)
+    }
   }
 }
 </script>
+<style>
+#menu {
+    background: #fff;
+    position: absolute;
+    z-index: 1;
+    top: 10px;
+    right: 10px;
+    border-radius: 3px;
+    width: 120px;
+    border: 1px solid rgba(0,0,0,0.4);
+    font-family: 'Open Sans', sans-serif;
+}
+
+#menu a {
+    font-size: 13px;
+    color: #404040;
+    display: block;
+    margin: 0;
+    padding: 0;
+    padding: 10px;
+    text-decoration: none;
+    border-bottom: 1px solid rgba(0,0,0,0.25);
+    text-align: center;
+}
+
+#menu a:last-child {
+    border: none;
+}
+
+#menu a:hover {
+    background-color: #f8f8f8;
+    color: #404040;
+}
+
+#menu a.active {
+    background-color: #3887be;
+    color: #ffffff;
+}
+
+#menu a.active:hover {
+    background: #3074a4;
+}
+</style>
